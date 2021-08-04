2e0ccecdd436cd75764407beb73626202ab45539
Mason Elliott <mason.elliott12@gmail.com>
2021-08-04 18:55:56 +1200

    push to github

--- src/server.js
+++ src/server.js
@@ -1,23 +1,57 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+/*const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-    //console.log('a user connected');
+  console.log("User Connected");
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
+});
 
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-    });
-    
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});*/
+
+/*const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+io.on('connection', () => {
+
+  console.log("hello");
+
+});
+server.listen(3000);*/
+
+const io = require('socket.io')(3000);
+
+io.on('connection', (socket) => {
+
+  console.log('client connected:', socket.client.id);
+
+  socket.on('MakeOffer', (data) => {
+    console.log('new message - MakeOffer:', data);
   });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+  socket.on('TakeOffer', (data) => {
+    console.log('new message - TakeOffer:', data);
+  });
+
+  socket.on('CancelOffer', (data) => {
+    console.log('new message - CancelOffer:', data);
+  });
+
+  socket.on('DepositToken', (data) => {
+    console.log('new message - DepositToken:', data);
+  });
+
+  socket.on('WithdrawToken', (data) => {
+    console.log('new message - WithdrawToken:', data);
+  });
+
 });
